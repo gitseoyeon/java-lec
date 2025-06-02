c6f89f7dfd6a088254d940a4c151d966d85d8f8a
gitseoyeon <tjdus553926@gmail.com>
2025-06-02 11:24:21 +0900

    commit: 증감 연산자

--- src/lec04/Operate.java
+++ src/lec04/Operate.java
@@ -20,19 +20,38 @@ public class Operate {
 //        System.out.println(remainder);
 
         // 복합 연산자
-        a += 5; // a = a + 5
+//        a += 5; // a = a + 5
+//        System.out.println(a);
+//
+//        b -= 1;
+//        System.out.println(b); // 2
+//
+//        b *= 4;
+//        System.out.println(b); // 8
+//
+//        b /= 3;
+//        System.out.println(b); // 2
+//
+//        b %= 2;
+//        System.out.println(b); // 0
+
+        // 증감 연산자
+        ++a;
         System.out.println(a);
 
-        b -= 1;
-        System.out.println(b); // 2
+        --b;
+        System.out.println(b);
 
-        b *= 4;
-        System.out.println(b); // 8
+        a--;
+        System.out.println(a);
 
-        b /= 3;
-        System.out.println(b); // 2
+        b--;
+        System.out.println(b);
 
-        b %= 2;
-        System.out.println(b); // 0
+        System.out.println(++a);
+        System.out.println(++b);
+
+        System.out.println(a++);
+        System.out.println(b++);
     }
 }
