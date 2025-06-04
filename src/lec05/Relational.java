4842fa7f7c6bb1c61cbd295a8509f31caf179745
gitseoyeon <tjdus553926@gmail.com>
2025-06-04 11:04:01 +0900

    commit: 관계 연산자

---
+++ src/lec05/Relational.java
@@ -0,0 +1,16 @@
+package lec05;
+
+public class Relational {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+
+        System.out.println("a > b: " + (a > b)); // true
+        System.out.println("a < b: " + (a < b)); // false
+        System.out.println("a >= 10: " + (a >= 10)); // true
+        System.out.println("b <= 5: " + (b <=5)); // false
+    }
+}
