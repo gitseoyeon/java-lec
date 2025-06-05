8d61c84fb26cf42c5bc2ec8fe904bb6784f1bd56
gitseoyeon <tjdus553926@gmail.com>
2025-06-05 14:26:59 +0900

    commit: 함수

---
+++ src/lec06/Function.java
@@ -0,0 +1,16 @@
+package lec06;
+
+public class Function {
+    public static void printStudent(String name, int age) {
+        System.out.println("이름: "+ name);
+        System.out.println("나이: " + age);
+        System.out.println("------------");
+    }
+    public static void main(String[] args) {
+        System.out.println("=== 학생 정보 출력 ===");
+        printStudent("bob", 13);
+        printStudent("Alice", 20);
+        printStudent("Charlie", 25);
+
+    }
+}
