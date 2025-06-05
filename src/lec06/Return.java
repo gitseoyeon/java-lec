f52cd01e4813e22e09ecdb4406a020df29344236
gitseoyeon <tjdus553926@gmail.com>
2025-06-05 15:02:43 +0900

    commit: return, void

---
+++ src/lec06/Return.java
@@ -0,0 +1,12 @@
+package lec06;
+
+public class Return {
+    public static int addNumber(int a, int b){
+        int result = a + b;
+        return result;
+    }
+    public static void main(String[] args) {
+        int number = addNumber(10, 7);
+        System.out.println(number);
+    }
+}
