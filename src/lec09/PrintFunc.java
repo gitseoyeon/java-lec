f568bc84f19c64b065680042285c623b8577a48c
gitseoyeon <tjdus553926@gmail.com>
2025-06-11 11:46:45 +0900

    commit: print 함수

---
+++ src/lec09/PrintFunc.java
@@ -0,0 +1,28 @@
+package lec09;
+
+public class PrintFunc {
+    public static void main(String[] args) {
+//        System.out.print("줄바꿈이 없는 프린트 함수");
+//        System.out.println("줄바꿈을 해본다.");
+//        System.out.println("줄바꿈");
+//        System.out.print("줄바꿈 안함");
+//        System.out.println("하이");
+
+//        String name = "kim";
+//        int num = 7;
+//        System.out.println((name + num).getClass().getName());
+//
+//        System.out.println("안녕\n하세여");
+//        System.out.println("반가\t워요");
+//
+//        int age = 100;
+//        double pi = 3.14159;
+//        String state = "배고프다";
+//        System.out.printf("나이: %d, 원주율: %.2f, 상태: %s", age, pi, state);
+
+        String name = "Alice";
+        int age = 28;
+        double height = 164.52;
+        System.out.printf("Name: %s, Age: %d, Height: %.1f", name, age, height);
+    }
+}
