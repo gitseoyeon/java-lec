5b61d08f6d1538c5962b8c7f43d8aa5008bac218
gitseoyeon <tjdus553926@gmail.com>
2025-06-12 23:33:26 +0900

    commit: 상속

---
+++ src/lec10/Inheritance.java
@@ -0,0 +1,51 @@
+package lec10;
+
+//class Animal {
+//    String name;
+//
+//    public Animal(String name){
+//        this.name = name;
+//    }
+//
+//    public void printName(String name){
+//        System.out.println("Animal name: " + name + this.name);
+//    }
+//}
+//
+//class Dog extends Animal { // Animal의 모든 필드와 메소드를 상속 받는다.
+//    String breed;
+//
+//    public Dog(String name, String breed) {
+//        super(name);
+//        this.breed = breed;
+//    }
+//}
+//
+//class Vehicle {
+//    String brand;
+//    public Vehicle(String brand){
+//        this.brand = brand;
+//    }
+//
+//    public void display(){
+//        System.out.println(brand);
+//    }
+//}
+//
+//class Car extends Vehicle {
+//    String model;
+//    public Car(String brand, String model){
+//        super(brand);
+//        this.model = model;
+//    }
+//}
+
+public class Inheritance {
+    public static void main(String[] args) {
+//        Dog dog = new Dog("멍멍이", "푸들");
+//        dog.printName("쿠키");
+//
+//        Car car = new Car("현대", "소나타");
+//        car.display();
+    }
+}
